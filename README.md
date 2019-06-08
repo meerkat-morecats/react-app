959e03e435de1c46603af3f4e9158fa37b9e87a5
kaik <kangkai@koolearn-inc.com>
2019-06-08 16:04:54 +0800

    installed commit

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 ### Readme.md
 
-### 任务清单 todolist
+### TODO
 
 - [x] 配置 vscode
 - [x] 配置 eslint
-- [ ] 配置 babel
+- [x] 配置 babel
 - [ ] 配置 webpack
-- [ ] 开发前端界面
+- [ ] 开发前端界面 hooks，实现多个页面，每个页面依赖多个组件
 - [ ] 开发 node 接口
-- [ ] express 缓存策略、文件压缩、node 打包
+- [ ] express 缓存策略、文件压缩、打包
 - [ ] 上线
-- [ ] 脚手架、实现状态分离
-- [ ] react ssr 、node server
+- [ ] webpack + 单个页面的 ssr
+- [ ] webpack + 多个页面的 ssr
+- [ ] 脚手架、实现页面组建的创建
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,10 +1,120 @@
 /**
  * @file webpack.common.js
  * ----------
- * File Created: Wednesday, 5th June 2019 11:03:10 pm
- * Last Modified: Wednesday, 5th June 2019 11:09:11 pm
+ * File Created: Saturday, 8th June 2019 11:18:55 am
+ * Last Modified: Saturday, 8th June 2019 3:22:02 pm
  * Modified By: kangkai (kakcool@qq.com)
  * ----------
  * @author kangkai
  * @description
  */
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const webpack = require("webpack");
+const packageInfo = require("../package");
+
+const ENV_PRODUCTION = "production";
+const ENV_DEVELOPMENT = "development";
+
+exports.ENV_DEVELOPMENT = ENV_DEVELOPMENT;
+exports.ENV_DEVELOPMENT = ENV_DEVELOPMENT;
+
+exports.getConfig = function() {
+  const cssLoader =
+    process.env.NODE_ENV === ENV_PRODUCTION
+      ? [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"]
+      : [
+          "css-hot-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader"
+        ];
+  const sassLoader =
+    process.env.NODE_ENV === ENV_PRODUCTION
+      ? [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      : [
+          "css-hot-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ];
+  const config = {
+    mode:
+      process.env.NODE_ENV === ENV_PRODUCTION
+        ? ENV_PRODUCTION
+        : ENV_DEVELOPMENT,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader"
+            },
+            {
+              loader: "eslint-loader",
+              options: {
+                quiet: true
+              }
+            }
+          ]
+        },
+        {
+          test: /\.css$/,
+          use: cssLoader
+        },
+        {
+          test: /\.sass$/,
+          use: sassLoader
+        },
+        {
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          use: ["file-loader"]
+        },
+        {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          use: ["url-loader"]
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg|ico)$/i,
+          use: ["file-loader", "image-webpack-loader"]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env": {
+          NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        },
+        "process.package": {
+          version: JSON.stringify(packageInfo.version)
+        }
+      })
+    ]
+  };
+  if (process.env.NODE_ENV === ENV_PRODUCTION) {
+    config.plugins.push(
+      new webpack.BannerPlugin({
+        banner:
+          "react-ssr version : " + packageInfo.version + " , file : [file]"
+      })
+    );
+
+    config.optimization = {
+      // minimize: process.env.NODE_ENV === ENV_PRODUCTION,
+      minimizer: [new OptimizeCSSAssetsPlugin()],
+      mergeDuplicateChunks: true
+    };
+  } else {
+    config.devtool = "cheap-module-eval-source-map";
+  }
+  return config;
+};
