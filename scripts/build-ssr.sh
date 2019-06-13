e3eb1a473a37096c42d7fd8db0cdf60bfe129859
kang kai <kangkai@koolearn-inc.com>
2019-06-13 19:06:11 +0800

    [feat]:add a simple webpack demo

--- scripts/build-ssr.sh
+++ scripts/build-ssr.sh
@@ -1,3 +1,2 @@
-rm -rf ../src/dist
 export NODE_ENV=production
 webpack --config=webpack/webpack.ssr.js --progess --quiet
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,2 @@
-rm -rf ../src/dist
 export NODE_ENV=production
 ./node_modules/.bin/webpack --config=webpack/webpack.prod.js --progess --quiet
